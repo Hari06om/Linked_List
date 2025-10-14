0c95a7f06c110ef44c69515e7d0919f5bf1fd6f3
Hari06om <hariohmverma96@gmail.com>
2025-10-14 18:15:10 +0530

    reveise the topic

---
+++ Intr.java
@@ -0,0 +1,14 @@
+public class Introduction{
+    public static class Node{
+        int data;
+        Node next;
+        public class Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String args[]){
+        System.out.println("This is a linked list introduction");
+    }
+}
