a3cd707628dee7310a7497a15b47dfe9ef918086
Hari06om <hariohmverma96@gmail.com>
2025-10-14 18:33:54 +0530

    revise the topic

--- AddinLast.java
+++ AddinLast.java
@@ -1,43 +1,48 @@
-public class AddinLast {
+public class AddinLast{
     public static class Node{
-        int data;
-        Node next;
-        public Node(int data){
-            this.data = data;
-            this.next = null;
-        }
+         int data;
+          Node next;
+           public Node(int data){
+            this.data= data;
+            this.next=null;
+           }
+
     }
-        public static Node head;
-        public static Node tail;
+    public static Node head;
+    public static Node tail;
+
     public static void addlast(int data){
-        
-        //step1: create a new node
-        Node newNode = new Node(data);
+        Node newNode= new Node(data);
         if(head == null){
-            head =tail=newNode; //if list is empty, new node will be both head and tail
+            head=tail=newNode;
             return;
         }
-        
-        //step2: NewNode will be the new head
-        tail.next = newNode; //link the new node to the current head
-        
-        //step3: update head to point to the new node
+
+        tail.next = newNode;
         tail = newNode;
+
     }
 
-    
-
-    public static void main(String args[]){
-        AddinLast ll = new AddinLast();
-        ll.addlast(85);
-        ll.addlast(30);
-        ll.addlast(50);
-        System.out.println("Linked List:");
+    public static void print(){
         Node temp = head;
-        while(temp!=null){
-            System.out.print(temp.data+" ");
+        while(temp != null){
+            System.out.print(temp.data + " -> ");
             temp = temp.next;
         }
-        System.out.println();
+        System.out.println("null");
+    }
+    public static void main(String args[]){
+        AddinLast ll = new AddinLast();
+        ll.addlast(2);
+        ll.addlast(3);
+        ll.addlast(4);
+        ll.addlast(5);
+        ll.addlast(6);
+        ll.addlast(7);
+
+        print();
+
+
+
     }
 }
