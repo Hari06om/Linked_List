7757d5b81772059331af0f2deaef1c5d97b051f6
Hari06om <hariohmverma96@gmail.com>
2026-01-01 21:31:39 +0530

    revise the topic

---
+++ Rev/classroom.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+
+public class classroom {
+    public static void main(String[] args){
+        //create
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        //add
+        ll.addLast(10);
+        ll.addLast(23);
+        ll.addFirst(12);
+        
+        int i = 2;
+        
+        for(int j = 3; j < 6; j++){
+            ll.add(j, i);
+            i += 2;
+            System.out.println(ll);
+        }
+
+        //remove
+        ll.removeLast();
+        ll.removeFirst();
+        System.out.println(ll);
+    }
+}
